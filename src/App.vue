763257f106726924700bacd707011baa5d102054
bug100000 <bug100000@outlook.com>
2018-11-21 14:57:10 +0800

    搭建页面结构大体完成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,102 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/img/logo_vue.png"> -->
+    <div id="yTop">
+      <span>admin</span>
+      <span>注销</span>
+    </div>
+    <div id="yBodyBox" ref="yBodyBox">
+      <el-row class="tac" id="yLeftNav">
+        <el-col style="width: 201px;">
+          <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" style="border: none;">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+              <el-menu-item index="1-3">选项3</el-menu-item>
+              <el-submenu index="1-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <i class="el-icon-setting"></i>
+              <span slot="title">导航四</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+      <div id="yBody" ref="yBody">
+        <router-view/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+    name: "App",
+    mounted: function() {
+        this.$refs.yBodyBox.style.height = Number(document.documentElement.clientHeight) - 60 + "px";
+        this.$refs.yBody.style.width = Number(document.documentElement.clientWidth) - 201 + "px";
+    },
+    methods: {
+        handleOpen(key, keyPath) {
+            console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /* text-align: center; */
+    color: #2c3e50;
+    /* margin-top: 60px; */
+}
+
+.el-menu-item {
+    border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+}
+
+#yTop {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 60px;
+    background-color: #ebebf7;
+}
+
+#yBodyBox {
+    position: relative;
+}
+
+#yLeftNav {
+    margin-top: 60px;
+    width: 201px;
+    height: 100%;
+    background-color: rgb(84, 92, 100);
+}
+
+#yBody {
+    position: absolute;
+    top: 0;
+    left: 201px;
+    height: 100%;
+    background-color: gray;
 }
 </style>
--- src/page/Login_v.vue
+++ src/page/Login_v.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="login">
+  <div id="login" class="login-box">
     <el-card class="box-card y-card">
-      <div slot="header" class="clearfix">
+      <div slot="header" class="clearfix y-card-header">
         <span>用户登录</span>
         <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
       </div>
@@ -12,12 +12,15 @@
         <el-form-item class="yLabel" label="密码" prop="pass">
           <el-input class="yInput" type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item class="yLabel">
           <el-button class="ySubmit yInput" type="primary" @click="submitForm('ruleForm')">提交</el-button>
           <!-- <el-button @click="resetForm('ruleForm')">重置</el-button> -->
         </el-form-item>
       </el-form>
     </el-card>
+    <div class="loginFooter">
+      Copyright © 2006-2017 Nest, All Rights Reserved
+    </div>
   </div>
 </template>
 
@@ -79,14 +82,44 @@ export default {
 }
 /* 自定义样式 */
 
+.loginFooter{
+    position: absolute;
+    bottom: 7vh;
+    margin-left: 50%;
+    transform: translateX(-50%);
+    color: #46556a;
+    font-size: 14px;
+    text-align: center;
+    font-weight: 800;
+    /* color: #a1a2a7; */
+    /* font-size: 12px; */
+    /* line-height: 27px; */
+}
+
+body{
+    margin: 0;
+}
+
+.login-box{
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    background-image: url(../assets/img/logBg.png);
+    background-position:center;
+}
+
 .y-card {
     float: right;
     margin-right: 5vw;
     margin-top: 30vh;
+    background-color: rgba(232,232,232,0.6) !important;
+    border: none !important;
 }
 
 .ySubmit {
     width: 100%;
+    background-color: #03202e;
+    border-color: #03202e;
 }
 
 .yLabel label{
@@ -96,6 +129,25 @@ export default {
 .yInput{
   margin-right: 50px;
 }
+
+.el-card__header{
+    border-bottom: 1px solid rgba(50,50,58,0.13) !important;
+}
+
+.yLabel .el-form-item__label{
+    width: 15% !important;
+}
+
+.yLabel .el-form-item__content {
+    margin-left: 15% !important;
+    width: 70%;
+}
+
+.yLabel .ySubmit {
+    background-color: #03202e;
+    border-color: #03202e;
+}
+
 /* 自定义样式 */
 
 /* 卡片样式 */
