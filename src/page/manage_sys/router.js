1f125041b5029d50b908251d558e7a4744ca998d
bug100000 <bug100000@outlook.com>
2019-01-09 16:57:58 +0800

    添加路由懒加载

--- src/page/manage_sys/router.js
+++ src/page/manage_sys/router.js
@@ -1,40 +1,55 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Home from './spa/Home.vue'
-import List from './spa/List.vue'
-import Upload from './spa/Upload.vue'
-import RichText from './spa/RichText.vue'
-import Vuex from './spa/Vuex.vue'
+// import Home from './spa/Home.vue'
+// import List from './spa/List.vue'
+// import Upload from './spa/Upload.vue'
+// import RichText from './spa/RichText.vue'
+// import Vuex from './spa/Vuex.vue'
+// const Home = r => require.ensure([], () => r(require('./spa/Home.vue')), '/')
+// const List = r => require.ensure([], () => r(require('./spa/List.vue')), '/List')
+// const Upload = r => require.ensure([], () => r(require('./spa/Upload.vue')), '/Upload')
+// const RichText = r => require.ensure([], () => r(require('./spa/RichText.vue')), '/RichText')
+// const Vuex = r => require.ensure([], () => r(require('./spa/Vuex.vue')), '/Vuex')
 
 Vue.use(Router)
+
+// 使用路由懒加载
+function loadView(view) {
+    return () => import(/* webpackChunkName: "view-[request]" */ `./spa/${view}.vue`)
+}
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: loadView("Home")
+      // component: Home
     },
     {
       path: '/list',
       name: 'List',
-      component: List
+      component: loadView("List")
+      // component: List
     },
     {
       path: '/upload',
       name: 'Upload',
-      component: Upload
+      component: loadView("Upload")
+      // component: Upload
     },
     {
       path: '/rich_text',
       name: 'RichText',
-      component: RichText
+      component: loadView("RichText")
+      // component: RichText
     },
     {
       path: '/vuex',
       name: 'Vuex',
-      component: Vuex
+      component: loadView("Vuex")
+      // component: Vuex
     }
   ]
 })
